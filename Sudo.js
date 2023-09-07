5cfeee53e44fb42fe2d1645fb3f3158177c843ea
JJ-debug1 <144392086+JJ-debug1@users.noreply.github.com>
2023-09-07 22:44:59 +0100

    Sudo1.js

--- Sudo.js
+++ Sudo.js
@@ -1,4 +1,4 @@
-const {SUDO} = require('../config');
+const{SUDO} = require('../config');
 const Heroku = require('heroku-client');
 const heroku = new Heroku({token: process.env.HEROKU_API_KEY});
 const baseURI = '/apps/' + process.env.HEROKU_APP_NAME;
